c7f713078a2eef8b9c68cc5a06800eea049a57ba
Sorin Dolha <SDolha@Hotmail.com>
2017-06-30 11:13:05 +0300

    Ep1.Salut

---
+++ Ep1.Salut/Ep1.Salut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ep1.Salut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cum va numiti?");
+            string numeUtilizator = Console.ReadLine();
+            Console.WriteLine($"Salut, {numeUtilizator}!");
+        }
+    }
+}
