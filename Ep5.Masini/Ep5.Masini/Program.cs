f4588162be971fd9662e5c75cf6fa1e9fb6df811
Sorin Dolha <SDolha@Hotmail.com>
2017-07-21 22:44:03 +0300

    Ep5.Masini - Distanță compusă, mai multe perechi timp-viteză

--- Ep5.Masini/Ep5.Masini/Program.cs
+++ Ep5.Masini/Ep5.Masini/Program.cs
@@ -10,15 +10,32 @@ namespace Ep5.Masini
     {
         static void Main(string[] args)
         {
-            double timp, viteză;
+            double[] timpi, viteze;
 
-            Console.Write("Introduceti timpul (h): ");
-            timp = double.Parse(Console.ReadLine());
+            Console.Write("Introduceti cate perechi de timp-viteza aveti: ");
+            int câte = int.Parse(Console.ReadLine());
 
-            Console.Write("Introduceti viteza (km/h): ");
-            viteză = double.Parse(Console.ReadLine());
+            timpi = new double[câte];
+            viteze = new double[câte];
 
-            double distanță = timp * viteză;
+            for (int i = 0; i < câte; i++)
+            {
+                Console.WriteLine($"Perechea {i + 1}:");
+
+                Console.Write("\tIntroduceti timpul (h):");
+                timpi[i] = double.Parse(Console.ReadLine());
+
+                Console.Write("\tIntroduceti viteza (km/h): ");
+                viteze[i] = double.Parse(Console.ReadLine());
+            }
+
+            double distanță = 0;
+            for (int i = 0; i < câte; i++)
+            {
+                double distanțăI = timpi[i] * viteze[i];
+                distanță += distanțăI;
+            }
+
             Console.WriteLine($"Se parcurge distanta de {distanță} km.");
         }
     }
