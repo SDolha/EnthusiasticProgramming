62efe538e9adb52b1219d96268130078ffc684b9
Sorin Dolha <SDolha@Hotmail.com>
2017-07-21 22:32:38 +0300

    Ep5.Masini - Distanța simplă

---
+++ Ep5.Masini/Ep5.Masini/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ep5.Masini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double timp, viteză;
+
+            Console.Write("Introduceti timpul (h): ");
+            timp = double.Parse(Console.ReadLine());
+
+            Console.Write("Introduceti viteza (km/h): ");
+            viteză = double.Parse(Console.ReadLine());
+
+            double distanță = timp * viteză;
+            Console.WriteLine($"Se parcurge distanta de {distanță} km.");
+        }
+    }
+}
